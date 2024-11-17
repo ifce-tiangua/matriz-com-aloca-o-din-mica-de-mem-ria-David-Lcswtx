f70797bc5e9879d7787f9dacd41462334d382065
David-Lcswtx <davidlucswtx@gmail.com>
2024-11-17 14:26:23 -0300

    matrizdinamicaaa

--- main.c
+++ main.c
@@ -1,9 +1,59 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int main(){
+int main()
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
-
-    return 0;
+{
+	
+	int **m; 
+	int a, b; // a = linha - b = coluna
+	int i, k; // elementos para os loops no for
+	
+	scanf("%d", &a);
+	scanf("%d", &b);
+	
+	if(a <= 0 || b <= 0){
+		
+		printf("[matriz vazia]\n");
+		
+		return 0;
+	}
+	else{
+	
+	//alocar memoria p matriz
+	m = (int **)malloc(a * sizeof(int *));
+	
+	for(i = 0; i < a; i++){
+		m[i] = malloc(b * sizeof(int));
+	}
+	
+	//acima alocacao - abaixo scan elementos
+	
+	for(i = 0; i < a; i++){
+        
+		for(k = 0; k < b; k++){
+			scanf("%d", &m[i][k]);
+		}
+	}
+	
+	//impressao dos elemnetos da matriz
+	for(i = 0; i < a; i++){
+        
+		for(k = 0; k < b; k++){
+			printf("%d ", m[i][k]);
+		}
+		printf("\n");
+	}
+	
+	//desacolar memoria
+	for(i = 0; i < a; i++){
+        
+		free(m[i]);
+	}
+	free (m);
+	
+	return 0;
+	
+	}
+	
 }
